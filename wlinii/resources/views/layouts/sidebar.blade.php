4be81693e5e9c0c1d5feaa098276f23339eeaf50
VladyCoder <Illia_bilyk_1012@outlook.com>
2020-09-15 22:17:14 +0800

    fix

--- wlinii/resources/views/layouts/sidebar.blade.php
+++ wlinii/resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="menu-sidebar d-none d-lg-block">
     <div class="logo">
-        <a href="/admin/dashboard">
+        <a href="{{ url('/dashboard') }}">
             ADMINISTRADOR DE WLINII
             <!-- <img src="images/icon/logo.png" alt="Cool Admin" /> -->
         </a>
