4e0d5546975577bdf914f79da94af540a5834803
Diego W <diegowarthon1190@gmail.com>
2020-09-21 11:56:31 -0500

    diego21

--- inmobiliaria/app/Http/Controllers/PublicationController.php
+++ inmobiliaria/app/Http/Controllers/PublicationController.php
@@ -361,6 +361,7 @@ class PublicationController extends Controller
             $image = DB::table('publicaciondetalleimagenes')->where('IdPubImage', $imageId)->first();
             $id = DB::table('publicaciondetalleimagenes')->where('IdPubImage', $imageId)->delete();
             //unlink($this->storage_path.$image->Des_url);
+            echo $this->storage_path.$image->Des_url;
             File::delete($this->storage_path.$image->Des_url);
         }catch(Throwable $e){
             return response()->json([
